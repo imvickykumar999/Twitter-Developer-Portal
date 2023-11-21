e7f15fc4a8541249df1658a7e76f87fd33e0f068
imvickykumar999 <imvickykumar999@gmail.com>
2023-11-21 18:25:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,15 +10,38 @@ with open('static/keys.json') as f:
 
 CONSUMER_KEY = API_keys['CONSUMER_KEY']
 CONSUMER_SECRET = API_keys['CONSUMER_SECRET']
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+
+auth = tweepy.OAuthHandler(
+    CONSUMER_KEY, 
+    CONSUMER_SECRET
+)
 
 ACCESS_TOKEN = API_keys['ACCESS_TOKEN']
 ACCESS_TOKEN_SECRET = API_keys['ACCESS_TOKEN_SECRET']
-auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+auth.set_access_token(
+    ACCESS_TOKEN, 
+    ACCESS_TOKEN_SECRET
+)
 
 API = tweepy.API(auth)
-try:
+
+try: 
     API.verify_credentials()
-    print("\nAuthentication OK")
-except:
-    print("\nError during authentication")
+    print('\nAuthentication OK.')
+
+except Exception as e: 
+    print('\n', e)
+
+# tweet = API.retweet(id = 1726946437991453064)
+# print(tweet)
+
+'''
+tweepy.errors.Forbidden: 403 Forbidden
+
+453 - You currently have access to a subset of Twitter API v2 endpoints and 
+limited v1.1 endpoints (e.g. media post, oauth) only. If you need access to 
+this endpoint, you may need a different access level. 
+
+You can learn more here: https://developer.twitter.com/en/portal/product
+'''
